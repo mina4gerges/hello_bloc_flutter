f4c427efa4a8af16a1fea776211d313596378a6a
mina.gerges <mina.gerges@isae.edu.lb>
2021-03-08 11:35:31 +0200

    alternative method (second method to listen to connectivity changes) to 'StreamSubscription' is using 'BlocListener'

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,44 +1,20 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-import '../../constants/enums.dart';
 import './internet_cubit.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   final InternetCubit internetCubit;
-  StreamSubscription internetStreamSubscription;
 
   CounterCubit({@required this.internetCubit})
-      : super(CounterState(counterValue: 0)) {
-    monitorInternetCubit();
-  }
-
-  StreamSubscription<InternetState> monitorInternetCubit() {
-    return internetStreamSubscription = internetCubit.listen((internetState) {
-      if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Wifi) {
-        increment();
-      } else if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Mobile) {
-        decrement();
-      }
-    });
-  }
+      : super(CounterState(counterValue: 0));
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
-
-  @override
-  Future<void> close() {
-    internetStreamSubscription.cancel();
-    return super.close();
-  }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -17,43 +17,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreen extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      state.wasIncremented == true
-                          ? 'Incremented!'
-                          : 'Decremented!',
+    return BlocListener<InternetCubit, InternetState>(
+      listener: (context, state) {
+        if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Wifi) {
+          context.read<CounterCubit>().increment();
+        } else if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Mobile) {
+          context.read<CounterCubit>().decrement();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        state.wasIncremented == true
+                            ? 'Incremented!'
+                            : 'Decremented!',
+                      ),
+                      duration: Duration(
+                        milliseconds: 300,
+                      ),
                     ),
-                    duration: Duration(
-                      milliseconds: 300,
-                    ),
-                  ),
-                );
-              },
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline4,
-                );
-              },
-            ),
-            BlocBuilder<InternetCubit, InternetState>(
+                  );
+                },
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4,
+                  );
+                },
+              ),
+              BlocBuilder<InternetCubit, InternetState>(
                 builder: (context, state) {
                   if (state is InternetConnected &&
                       state.connectionType == ConnectionType.Wifi) {
@@ -61,56 +71,56 @@ class _HomeScreen extends State<HomeScreen> {
                   } else if (state is InternetConnected &&
                       state.connectionType == ConnectionType.Mobile) {
                     return Text("Mobile");
-                  }
-                  else if (state is InternetDisconnected) {
+                  } else if (state is InternetDisconnected) {
                     return Text("Disconnected");
-                  }
-                  else {
+                  } else {
                     return CircularProgressIndicator();
                   }
-                }),
-            Column(
-              children: [
-                ElevatedButton(
-                  child: Text("Go to second screen"),
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed('/secondScreen'),
-                ),
-                ElevatedButton(
-                  child: Text("Go to third screen"),
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed('/thirdScreen'),
-                ),
-              ],
+                },
+              ),
+              Column(
+                children: [
+                  ElevatedButton(
+                    child: Text("Go to second screen"),
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed('/secondScreen'),
+                  ),
+                  ElevatedButton(
+                    child: Text("Go to third screen"),
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed('/thirdScreen'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FloatingActionButton(
+                onPressed: () =>
+                    BlocProvider.of<CounterCubit>(context).increment(),
+                // onPressed: () => context.read<CounterCubit>().increment(),
+                tooltip: 'Increment',
+                heroTag: "btn1",
+                child: Icon(Icons.add),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FloatingActionButton(
+                // onPressed: () => BlocProvider.of<CounterCubit>(context).decrement(),
+                onPressed: () => context.read<CounterCubit>().decrement(),
+                heroTag: "btn2",
+                tooltip: 'Decrement',
+                child: Icon(Icons.remove),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FloatingActionButton(
-              onPressed: () =>
-                  BlocProvider.of<CounterCubit>(context).increment(),
-              // onPressed: () => context.read<CounterCubit>().increment(),
-              tooltip: 'Increment',
-              heroTag: "btn1",
-              child: Icon(Icons.add),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FloatingActionButton(
-              // onPressed: () => BlocProvider.of<CounterCubit>(context).decrement(),
-              onPressed: () => context.read<CounterCubit>().decrement(),
-              heroTag: "btn2",
-              tooltip: 'Decrement',
-              child: Icon(Icons.remove),
-            ),
-          ),
-        ],
       ),
     );
   }
