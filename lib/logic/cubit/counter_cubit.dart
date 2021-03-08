e616ce73a2e45faf6fc07e87e1d44939ab775427
mina.gerges <mina.gerges@isae.edu.lb>
2021-03-08 12:48:46 +0200

    alternative method (second method to listen to connectivity changes) to 'StreamSubscription' is using 'BlocListener'

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -2,15 +2,10 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-import './internet_cubit.dart';
-
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  final InternetCubit internetCubit;
-
-  CounterCubit({@required this.internetCubit})
-      : super(CounterState(counterValue: 0));
+  CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           create: (context) => InternetCubit(connectivity: connectivity),
         ),
         BlocProvider<CounterCubit>(
-          create: (context) => CounterCubit(internetCubit: context.read<InternetCubit>()),
+          create: (context) => CounterCubit(),
         ),
       ],
       child: MaterialApp(
